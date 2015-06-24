9447a9f00f0491b115678b80163733e1ba175b39
danielefenix <daniele.salvatore@fao.org>
2015-06-24 16:41:33 +0200

    animations on home page

--- scripts/index/host.js
+++ scripts/index/host.js
@@ -1,10 +1,17 @@
 /*global define*/
 define([
+    'jquery',
     'fx-menu/start',
-    'host/config'
-], function (TopMenu, C) {
+    'host/config',
+    'progressbar'
+], function ($, TopMenu, C, ProgressBar) {
 
     'use strict';
+
+    var s = {
+        PERCENTAGE_ONE : '#percentage-one',
+        PERCENTAGE_TWO : '#percentage-two'
+    };
 
     function Host() {
     }
@@ -12,7 +19,6 @@ define([
     Host.prototype.initFenixComponent = function () {
 
         this.initPageStructure();
-
 
         this.topMenu = new TopMenu({
             url: C.TOP_MENU,
@@ -25,15 +31,54 @@ define([
 
     Host.prototype.initPageStructure = function () {
 
-
-        <!-- Menu Toggle Script -->
-
         $("#menu-toggle").click(function(e) {
             e.preventDefault();
             $("#wrapper").toggleClass("toggled");
         });
 
+        this.initPercentageAnimations();
+
     };
+
+    Host.prototype.initPercentageAnimations = function () {
+
+        var circle = new ProgressBar.Circle(s.PERCENTAGE_ONE, {
+            color: '#FCB03C',
+            strokeWidth: 3,
+            trailWidth: 1,
+            duration: 1500,
+            ease : 'easeIn',
+            text: {
+                value: '0'
+            },
+            step: function(state, bar) {
+                bar.setText((bar.value() * 100).toFixed(0));
+            }
+        });
+
+        circle.animate(0.56);
+
+        var circle_two = new ProgressBar.Circle(s.PERCENTAGE_TWO, {
+            color: '#FCB03C',
+            strokeWidth: 3,
+            trailWidth: 1,
+            duration: 1500,
+            ease : 'easeOut',
+            text: {
+                value: '0'
+            },
+            step: function(state, bar) {
+                bar.setText((bar.value() * 100).toFixed(0));
+            }
+        });
+
+        circle_two.animate(0.92);
+
+    };
+
+
+
+
 
     return Host;
 
--- scripts/mains/index.js
+++ scripts/mains/index.js
@@ -24,6 +24,7 @@ require([
                     handlebars: "{FENIX_CDN}/js/handlebars/2.0.0/handlebars",
                     amplify: '{FENIX_CDN}/js/amplify/1.1.2/amplify.min',
                     rsvp: '{FENIX_CDN}/js/rsvp/3.0.17/rsvp',
+                    progressbar : '{FENIX_CDN}/js/progressbar/0.8.1/progressbar.min',
 
                     'host/config' : '../../config/config',
 
